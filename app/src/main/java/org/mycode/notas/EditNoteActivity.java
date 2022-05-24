0ef931f4f8148e076238358f2b1633b7238068ca
Iyonissio <mbryoyo01@gmail.com>
2022-05-24 16:27:13 +0200

    1 commit

---
+++ app/src/main/java/org/mycode/notas/EditNoteActivity.java
@@ -0,0 +1,71 @@
+package org.mycode.notas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+
+import java.util.HashSet;
+
+public class EditNoteActivity extends AppCompatActivity {
+
+    int noteId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId", -1);
+
+        if (noteId != -1) {
+            editText.setText(MainActivity.notes.get(noteId));
+        } else {
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() - 1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.notes.set(noteId, String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("package org.mycode.notas", Context.MODE_PRIVATE);
+
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+
+                sharedPreferences.edit().putStringSet("notes",set).apply();
+            }
+
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        });
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
